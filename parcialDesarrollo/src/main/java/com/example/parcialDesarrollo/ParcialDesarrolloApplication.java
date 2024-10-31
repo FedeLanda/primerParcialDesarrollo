c4c2cbd0c536b20823557518c87f213a58d8197e
FedeLanda <100427585+FedeLanda@users.noreply.github.com>
2024-10-31 11:56:49 -0300

    Add files via upload

---
+++ parcialDesarrollo/src/main/java/com/example/parcialDesarrollo/ParcialDesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcialDesarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialDesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialDesarrolloApplication.class, args);
+	}
+
+}
